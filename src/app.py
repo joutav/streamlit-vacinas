79089ff1c968f7b8ff26b08f0d5df79c1e9b6f5d
joutav <jvo.maga@gmail.com>
2021-01-26 19:05:17 -0300

    update

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ def carrega_dados_kaggle():
     bash_command = 'kaggle datasets download -d gpreda/covid-world-vaccination-progress'
     process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
-    return output
+    return output, error
 
 
 def main():
