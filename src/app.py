293d1c9642e2cad7faaa441d0ce0ace40bd924f5
joutav <jvo.maga@gmail.com>
2021-01-26 19:20:23 -0300

    update

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import subprocess
 import zipfile
+import os
 
 
 def baixar_dados():
@@ -10,13 +11,18 @@ def baixar_dados():
     return output, error
 
 
+def baixar_dados_2():
+    os.system('kaggle datasets download -d gpreda/covid-world-vaccination-progress')
+    return 'done'
+
+
 def extrair_dados():
     with zipfile.ZipFile('./covid-world-vaccination-progress.zip', 'r') as zip_ref:
         zip_ref.extractall('./dados/')
 
 
 def main():
-    st.text(baixar_dados())
+    st.text(baixar_dados_2())
     extrair_dados()
 
 
